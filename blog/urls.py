3e7737f29d7b06d428788d5214ab5318e2368fa4
zionhuang <huangdaoziwen@gamil.com>
2017-09-14 23:59:02 +0800

    blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^index/$', views.index),
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
+    url(r'^edit/(?P<article_id>[0-9]+)$', views.edit, name='edit'),
+    url(r'^create/$', views.create, name='create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from . import models
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
+
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article.html', {'article': article})
+
+
+def edit(request, article_id):
+    if '0' == str(article_id):
+        return render(request, 'blog/edit.html')
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/edit.html', {'article': article})
+
+
+def create(request):
+    title = request.POST.get('title', 'Title')
+    content = request.POST.get('content', 'Content')
+    article_id = request.POST.get('article_id', '0')
+
+    if '0' == article_id:
+        models.Article.objects.create(title=title, content=content)
+        return HttpResponseRedirect('/blog/index')
+    article = models.Article.objects.get(pk=article_id)
+    article.title = title
+    article.content = content
+    article.save()
+    return HttpResponseRedirect('/blog/index')
